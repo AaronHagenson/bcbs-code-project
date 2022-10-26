e05710ae2fa6586e5280e2bfd174ca8e4d74cce0
Aaron <thedestroyer9876@yahoo.com>
2022-10-26 16:03:49 -0500

    create POST endpoint in Express server to return GraphQL data

--- express-server/index.js
+++ express-server/index.js
@@ -1,4 +1,6 @@
-const express = require("express");
+const express = require("express"); 
+const gql = require("graphql-request").gql;
+const request = require("graphql-request").request;
 
 const app = express();
 
@@ -7,6 +9,30 @@ const PORT = process.env.PORT || 4000;
 app.get('/', (req, res)=>{
     res.status(200);
     res.send("BCBS server - get successful");
+});
+
+app.post('/media', (req, res) => {
+    const query = gql`  
+        {
+            Page(page: 1, perPage: 10) {
+            media(sort: POPULARITY_DESC) {
+                id
+                title {
+                    english
+                }
+                description
+                coverImage {
+                    large
+                }
+            }
+        }      
+    }`;
+
+    request('https://graphql.anilist.co', query).then((data) => {
+        res.send(data);
+    }).catch((err) => {
+        res.send(err);
+    });
 });
 
 app.listen(PORT, (error) =>{
